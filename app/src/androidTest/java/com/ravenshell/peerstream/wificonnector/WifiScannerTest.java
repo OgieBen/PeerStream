d7bb94d8095f86890a9dfb2f50419006e1586327
ogie Ben <bensoft2k5@gmail.com>
2018-11-20 17:05:05 +0100

    added mockito fro testing

--- app/src/androidTest/java/com/ravenshell/peerstream/wificonnector/WifiScannerTest.java
+++ app/src/androidTest/java/com/ravenshell/peerstream/wificonnector/WifiScannerTest.java
@@ -2,10 +2,13 @@ package com.ravenshell.peerstream.wificonnector;
 
 import android.content.Context;
 
+import com.ravenshell.peerstream.wificonnector.scanner.Scanner;
 import com.ravenshell.peerstream.wificonnector.scanner.WifiScanner;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Captor;
+import org.mockito.MockitoAnnotations;
 
 /**
  * Created by ogie on 11/19/2018.
@@ -14,17 +17,26 @@ public class WifiScannerTest {
 
 //    @Mock
     Context context;
+    private WifiScanner mWifiScanner;
+
+    @Captor
+    Scanner.Callback mScannerCallback;
 
     @Before
     public void setUp() throws Exception {
-
-        WifiScanner wifiScanner = new WifiScanner(context);
+         MockitoAnnotations.initMocks(this);
+         mWifiScanner = new WifiScanner(context);
 
         //wifiScanner.setupWifiManager();
     }
 
     @Test
     public void beginSearch() throws Exception {
+
+        mWifiScanner.setupWifiManager(mScannerCallback);
+
+
+
     }
 
     @Test

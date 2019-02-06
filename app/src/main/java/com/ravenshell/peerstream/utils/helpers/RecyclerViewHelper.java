a6fc2216df94a73e52c74005d2fa46c2c134c0ba
ogie Ben <bensoft2k5@gmail.com>
2019-02-06 22:34:43 +0100

    initial commit

--- app/src/main/java/com/ravenshell/peerstream/utils/helpers/RecyclerViewHelper.java
+++ app/src/main/java/com/ravenshell/peerstream/utils/helpers/RecyclerViewHelper.java
@@ -1,4 +1,16 @@
 package com.ravenshell.peerstream.utils.helpers;
 
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ravenshell.peerstream.R;
+
 public class RecyclerViewHelper {
+
+    public static void updateDeviceListView(View view, WifiP2pDevice device) {
+
+        TextView deviceNameTextView = view.findViewById(R.id.device_name);
+        deviceNameTextView.setText(device.deviceName.toString());
+    }
 }
